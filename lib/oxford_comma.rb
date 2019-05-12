2c2761b6cbf4e154a4df4dbf23b7e0a966ec6d1c
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 03:55:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-  return array.join(" and ") if array.length == 2
-  return array.join(", ") + " and " + array[-1] if array.lenth > 3
+  return array.join(" and ") 
+  if array.lenth > 2
+    array.join(", ") + "and" + array[-1]
+  elsif array.length == 2
+    array.join(" and ")
+  
 end
