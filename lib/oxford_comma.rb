66c73a85ebecfca766bf4ad0e7c403d7f6287edb
Marc Poppy <poppy.marc@gmail.com>
2019-05-12 04:00:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ array = ["marc", "julie", "mango"]
 
 def oxford_comma(array)
   if array.length > 2
-    array.join(", ") + " and " + array[-1]
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
   elsif array.length == 2
     array.join(" and ")
   else
